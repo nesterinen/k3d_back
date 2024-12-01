74a706b29aac25d57b07dadc5fe83c5fcdc93c61
Aleksei Nesterinen <aleksei.nesterinen@outlook.com>
2024-12-02 00:10:04 +0200

    wcs service

--- src/controllers/wcsV1.ts
+++ src/controllers/wcsV1.ts
@@ -4,6 +4,8 @@ import axios from 'axios'
 import { WGS84toETRS89, bboxFromETRS89 } from '../utils/projections'
 import Joi from 'joi'
 
+// https://www.maanmittauslaitos.fi/ortokuvien-ja-korkeusmallien-kyselypalvelu/tekninen-kuvaus
+
 interface requestSchemaProps{
     latitude: number,
     longitude: number,
@@ -15,23 +17,24 @@ const requestSchema = Joi.object<requestSchemaProps>({
     size: Joi.number().min(500).max(2000),
 })
 
-// https://www.maanmittauslaitos.fi/ortokuvien-ja-korkeusmallien-kyselypalvelu/tekninen-kuvaus
-type coverageType = 'korkeusmalli_2m' | 'ortokuva_vari' |  'ortokuva_mustavalko' | 'ortokuva_vaaravari'
 
+type coverageType = 'korkeusmalli_2m' | 'ortokuva_vari' | 'ortokuva_vaaravari' // |  'ortokuva_mustavalko'
 
-const getWCS = (bbox: number[], coverage: coverageType) => {
+const getWCS = async (bbox: number[], coverage: coverageType)=> {
     const baseUrl = 'https://avoin-karttakuva.maanmittauslaitos.fi/ortokuvat-ja-korkeusmallit/wcs/v2?service=WCS&version=2.0.1&request=GetCoverage'
     const type = `&CoverageID=${coverage}`
     const easting = `&SUBSET=E(${bbox[0]},${bbox[2]})`
     const northing = `&SUBSET=N(${bbox[1]},${bbox[3]})`
     const format = '&format=image/tiff'
     //const compression = '&geotiff:compression=LZW'
-    //const scale = '&SCALEFACTOR=0.5'
 
-    // normal sizes
-    // korkeus2m 500x500, orto 2000x2000
+    // normal sizes: korkeus2m 500x500, orto 2000x2000
+    let scale = '&SCALEFACTOR=1'
+    if (coverage == 'ortokuva_vari' || coverage == 'ortokuva_vaaravari') {
+        scale = '&SCALEFACTOR=0.25' // turn 1km2 to 500x500 = 1px = 2m
+    }
 
-    const url = baseUrl + type + easting + northing + format
+    const url = baseUrl + type + easting + northing + format + scale
 
     const response = axios.get(
         url,
@@ -59,8 +62,9 @@ const wcsBBOXrequest = async (req: Request, res: Response, coverage: coverageTyp
 
     try {
         const tifFile = await getWCS(bbox, coverage)
+
         res.setHeader('Content-Type', 'image/tiff')
-        res.setHeader('Content-Disposition', 'attachment; filename=img.tif')
+        res.setHeader('Content-Disposition', 'attachment; filename=img.tiff')
         res.send(Buffer.from(tifFile, 'ascii')) //, 'utf-16le'
     } catch (error) {
         res.status(400).json({'error': error})
@@ -76,6 +80,48 @@ wcsV1.post('/korkeusmalli_2m', (req: Request, res: Response) => {
 
 wcsV1.post('/ortokuva_vari', (req: Request, res: Response) => {
     wcsBBOXrequest(req, res, 'ortokuva_vari')
+})
+
+wcsV1.post('/ortokuva_vaaravari', (req: Request, res: Response) => {
+    wcsBBOXrequest(req, res, 'ortokuva_vaaravari')
+})
+
+// Description responses
+
+wcsV1.get('/korkeusmalli_2m', (req: Request, res: Response) => {
+    res.json({
+        inputs: requestSchema.describe(),
+        output: {
+            style: 'Digital Elevation Model',
+            fileType: '.tiff',
+            'Content-Type': 'image/tiff',
+            sizeOfPixel: '2m'  
+        }
+    })
+})
+
+wcsV1.get('/ortokuva_vari', (req: Request, res: Response) => {
+    res.json({
+        inputs: requestSchema.describe(),
+        output: {
+            style: 'orthoimage',
+            fileType: '.tiff',
+            'Content-Type': 'image/tiff',
+            sizeOfPixel: '2m'  
+        }
+    })
+})
+
+wcsV1.get('/ortokuva_vaaravari', (req: Request, res: Response) => {
+    res.json({
+        inputs: requestSchema.describe(),
+        output: {
+            style: 'orthoimage',
+            fileType: '.tiff',
+            'Content-Type': 'image/tiff',
+            sizeOfPixel: '2m'  
+        }
+    })
 })
 
 export default wcsV1
